7c500f67767384624b260b469a99438c2f3b9d52
Nehu17 <yadavn1710@gmail.com>
2024-02-19 16:28:44 +0530

    added login and signup backend

--- Backend/src/routes/users.route.js
+++ Backend/src/routes/users.route.js
@@ -3,18 +3,8 @@ const express = require('express');
 const usersController = require('../controllers/users.controller.js');
 const router = express.Router();
 
-console.log('hello')
-router.post( "/form",usersController.form);
-router.post( "/saveAsDraft",usersController.saveAsDraft);
 
-router.get( "/submitdata",usersController.submitdata);
-
-router.get( "/draftdata",usersController.draftdata);
-
-router.get( "/typenametable",usersController.typenametable);
-//router.post( "/login",usersController.login);
-router.post('/login', usersController.loginUser);
 
 router.post( "/signup",usersController.signup);
-
+router.post( "/login",usersController.login);
 module.exports = router;
